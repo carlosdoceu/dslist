7da2cecfc84bbd09b9aef39003a691b8a3fae640
carlosdoceu <carlosodmen@gmail.com>
2024-11-11 16:38:17 -0300

    CHORE:entities

---
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -0,0 +1,175 @@
+package com.devsuperior.dslist.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_game")
+public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String genre;
+
+    @Column(name = "game_year")
+    private Integer year;
+
+    private String platform;
+    private String imgUrl;
+
+    private String shortDescription;
+
+    private String longDescription;
+
+    public Game(Long id, String title, String genre, Integer year, String platform, String imgUrl,
+            String shortDescription,
+            String longDescription) {
+        this.id = id;
+        this.title = title;
+        this.genre = genre;
+        this.year = year;
+        this.platform = platform;
+        this.imgUrl = imgUrl;
+        this.shortDescription = shortDescription;
+        this.longDescription = longDescription;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((title == null) ? 0 : title.hashCode());
+        result = prime * result + ((genre == null) ? 0 : genre.hashCode());
+        result = prime * result + ((year == null) ? 0 : year.hashCode());
+        result = prime * result + ((platform == null) ? 0 : platform.hashCode());
+        result = prime * result + ((imgUrl == null) ? 0 : imgUrl.hashCode());
+        result = prime * result + ((shortDescription == null) ? 0 : shortDescription.hashCode());
+        result = prime * result + ((longDescription == null) ? 0 : longDescription.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Game other = (Game) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (title == null) {
+            if (other.title != null)
+                return false;
+        } else if (!title.equals(other.title))
+            return false;
+        if (genre == null) {
+            if (other.genre != null)
+                return false;
+        } else if (!genre.equals(other.genre))
+            return false;
+        if (year == null) {
+            if (other.year != null)
+                return false;
+        } else if (!year.equals(other.year))
+            return false;
+        if (platform == null) {
+            if (other.platform != null)
+                return false;
+        } else if (!platform.equals(other.platform))
+            return false;
+        if (imgUrl == null) {
+            if (other.imgUrl != null)
+                return false;
+        } else if (!imgUrl.equals(other.imgUrl))
+            return false;
+        if (shortDescription == null) {
+            if (other.shortDescription != null)
+                return false;
+        } else if (!shortDescription.equals(other.shortDescription))
+            return false;
+        if (longDescription == null) {
+            if (other.longDescription != null)
+                return false;
+        } else if (!longDescription.equals(other.longDescription))
+            return false;
+        return true;
+    }
+
+}
