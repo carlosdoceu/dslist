57ada8520d87250a264c19c414991e067d297e8d
carlosdoceu <carlosodmen@gmail.com>
2024-11-11 18:07:07 -0300

    FEAT:Controller, Service, Repository

---
+++ src/main/java/com/devsuperior/dslist/controller/GameController.java
@@ -0,0 +1,37 @@
+package com.devsuperior.dslist.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dslist.DTO.GameMinDTO;
+import com.devsuperior.dslist.service.GameService;
+
+
+
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameController {
+
+    @Autowired
+    private GameService gameService;
+    
+   
+    
+    @GetMapping
+    public List<GameMinDTO> getAllGames() throws Exception{
+        try {
+            List<GameMinDTO> listaGame = gameService.getAllGames();
+            return listaGame;    
+        } catch (Exception e) {
+            throw new Exception(e);
+        }
+        
+    }
+    
+
+}
