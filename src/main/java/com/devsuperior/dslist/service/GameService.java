9de704ced6b3b6435de427ff532025b3dc05105b
carlosdoceu <carlosodmen@gmail.com>
2024-11-12 14:16:25 -0300

    FEAT: Query Consulta SQL no Spring Data JPA para Games List projections

--- src/main/java/com/devsuperior/dslist/service/GameService.java
+++ src/main/java/com/devsuperior/dslist/service/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.dslist.DTO.GameDTO;
 import com.devsuperior.dslist.DTO.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameRepository;
 
 @Service
@@ -17,17 +18,26 @@ public class GameService {
   @Autowired
   private GameRepository gameRepository;
 
+
+
+  @Transactional(readOnly = true)
+  public GameDTO findbyIdGame(Long id) {
+    Game game = gameRepository.findById(id).get();
+    GameDTO dto = new GameDTO(game);
+    return dto;
+  }
+
   public List<GameMinDTO> getAllGames() {
     List<Game> listaGames = gameRepository.findAll();
     List<GameMinDTO> dto = listaGames.stream().map(x -> new GameMinDTO(x)).toList();
     return dto;
   }
 
-  @Transactional(readOnly=true)
-  public GameDTO findbyIdGame(Long id) {
-    Game game = gameRepository.findById(id).get();
-    GameDTO dto = new GameDTO(game);
-    return dto;
+  @Transactional(readOnly = true)
+  public List<GameMinDTO> findByList(Long listId) {
+    List<GameMinProjection> result = gameRepository.searchByList(listId);
+    return result.stream().map(x->new GameMinDTO(x)).toList();
+
   }
 
 }
